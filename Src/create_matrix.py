820c9f2abe09eef831c4e04387c95648ea94a2eb
Mark Vassell <mdvy96@mail.missouri.edu>
2018-05-06 15:55:07 -0500

    Added result

--- Src/create_matrix.py
+++ Src/create_matrix.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+import seaborn as sb
+
 
 if __name__ == '__main__':
     binned_data = pd.read_csv('../Results/chromo_7_1MB.txt', sep='\t', header=None)
@@ -12,25 +14,30 @@ if __name__ == '__main__':
     size = max(max(x),max(y))
     print(size+1)
 
-    list_matrix = [[0] * (size+1) for i in range(size+1)]
+    list_matrix = [[0] * (size) for i in range(size)]
 
-
-    plt.imshow(list_matrix, cmap='hot', interpolation='nearest')
-    plt.show()
     i = 0
 
     while i < len(x):
         string = str(x[i]) + '\t' +str(y[i]) + '\t' + str(matrix_data[i])
         print(string)
-        list_matrix[x[i]][y[i]] = matrix_data[i]
+        list_matrix[x[i]-1][y[i]-1] = matrix_data[i]
 
         i += 1
 
-    plt.imshow(list_matrix, cmap='hot', interpolation='nearest')
-    plt.show()
 
 
-    #initial_matrix = binned_data.values
 
-    #print(initial_matrix)
 
+    for i in range(0, (int(len(list_matrix)))):
+        for j in range(i + 1, len(list_matrix)):
+            list_matrix[j][i]= list_matrix[i][j]
+
+    out_matrix = np.asanyarray(list_matrix)
+    # Accent, Accent_r, Blues, Blues_r, BrBG, BrBG_r, BuGn, BuGn_r, BuPu, BuPu_r, CMRmap, CMRmap_r, Dark2, Dark2_r, GnBu, GnBu_r, Greens, Greens_r, Greys, Greys_r, OrRd, OrRd_r, Oranges, Oranges_r, PRGn, PRGn_r, Paired, Paired_r, Pastel1, Pastel1_r, Pastel2, Pastel2_r, PiYG, PiYG_r, PuBu, PuBuGn, PuBuGn_r, PuBu_r, PuOr, PuOr_r, PuRd, PuRd_r, Purples, Purples_r, RdBu, RdBu_r, RdGy, RdGy_r, RdPu, RdPu_r, RdYlBu, RdYlBu_r, RdYlGn, RdYlGn_r, Reds, Reds_r, Set1, Set1_r, Set2, Set2_r, Set3, Set3_r, Spectral, Spectral_r, Wistia, Wistia_r, YlGn, YlGnBu, YlGnBu_r, YlGn_r, YlOrBr, YlOrBr_r, YlOrRd, YlOrRd_r, afmhot, afmhot_r, autumn, autumn_r, binary, binary_r, bone, bone_r, brg, brg_r, bwr, bwr_r, cividis, cividis_r, cool, cool_r, coolwarm, coolwarm_r, copper, copper_r, cubehelix, cubehelix_r, flag, flag_r, gist_earth, gist_earth_r, gist_gray, gist_gray_r, gist_heat, gist_heat_r, gist_ncar, gist_ncar_r, gist_rainbow, gist_rainbow_r, gist_stern, gist_stern_r, gist_yarg, gist_yarg_r, gnuplot, gnuplot2, gnuplot2_r, gnuplot_r, gray, gray_r, hot, hot_r, hsv, hsv_r, icefire, icefire_r, inferno, inferno_r, jet, jet_r, magma, magma_r, mako, mako_r, nipy_spectral, nipy_spectral_r, ocean, ocean_r, pink, pink_r, plasma, plasma_r, prism, prism_r, rainbow, rainbow_r, rocket, rocket_r, seismic, seismic_r, spring, spring_r, summer, summer_r, tab10, tab10_r, tab20, tab20_r, tab20b, tab20b_r, tab20c, tab20c_r, terrain, terrain_r, viridis, viridis_r, vlag, vlag_r, winter, winter_r
+
+    plt.imshow(out_matrix, cmap='BrBG')
+    plt.savefig('../Results/contact_matrix_before_norm.png')
+    #plt.show()
+    plt.close()
+    print(out_matrix)
